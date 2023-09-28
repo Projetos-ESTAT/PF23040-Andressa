a1dc5ae284b1b54d29e7891a924095aebb1ed214
Lucas Fonseca Alves <lukitasdaestatistica@gmail.com>
2023-09-28 01:03:03 -0300

    Finalização dos gráficos e análises

--- rdocs/lucas-fonseca.R
+++ rdocs/lucas-fonseca.R
@@ -27,11 +27,10 @@ source("rdocs/source/packages.R")
 
 #Puxando o banco
 
-cad_andressa <- read_csv("banco/cad_andressa.csv")
+cad_andressa <- read_csv("banco/cad_andressa_att.csv")
 
 #Mudando nome das variáveis 
-colnames(cad_andressa) <- c("data_hora", 
-                                "Pergunta_01",
+colnames(cad_andressa) <- c(    "Pergunta_01",
                                 "Pergunta_02",
                                 "Pergunta_03",
                                 "Pergunta_04",
@@ -52,10 +51,18 @@ colnames(cad_andressa) <- c("data_hora",
                                 "Pergunta_19"
 )
 
+
+#Filtrando membros que responderam que não possuiam curso, pois a cliente queria saber a quantidade: 53
+cad_andressa %>% 
+  filter((Pergunta_06 %in% c("Não possuo."))) %>% 
+  count()
+
 #Retira linhas que tenha CTE e não possuo na pergunta 06, a pedido da cliente
 #Foi de 160 linhas para 107 linhas
 cad_andressa <- cad_andressa %>%
   filter(!(Pergunta_06 %in% c("CTE", "Não possuo.")))
+
+
 
 #Estabelecendo Caminho para salvar os gráficos
 caminho_lucas <- "resultados"
@@ -97,7 +104,7 @@ ggplot(pergunta04) +
   ) +
   labs(x = "Função", y = "Frequência") +
   theme_estat()+
-  scale_y_continuous(breaks = seq(from = 0, to = 100, by = 10), limits=c(0, 100))
+  scale_y_continuous(breaks = seq(from = 0, to = 150, by = 50), limits=c(0, 150))
 ggsave(filename = file.path(caminho_lucas, "colunas-uni-freq-04.pdf"), width = 158, height = 93, units = "mm")
 
 #-------------------quais segmentos ocorreram mais lesões----------------------
@@ -161,12 +168,13 @@ ggplot(contagemSegmento) +
   geom_text(
     position = position_dodge(width = .9),
     vjust = -0.5, # hjust = .5,
-    size = 2.5
+    size = 2
   ) +
   labs(x = "Segmento", y = "Frequência") +
   theme_estat()+
-  scale_y_continuous(breaks = seq(from = 0, to = 70, by = 10), limits=c(0, 70))+
-  scale_x_discrete(guide = guide_axis(n.dodge = 2))
+  scale_y_continuous(breaks = seq(from = 0, to = 100, by = 20), limits=c(0, 110))+
+  scale_x_discrete(guide = guide_axis(n.dodge = 2))+
+  theme(axis.text.x = element_text(size = 8))
 ggsave(filename = file.path(caminho_lucas, "colunas-uni-freq-10.pdf"), width = 158, height = 93, units = "mm")
 
 
@@ -208,7 +216,7 @@ ggplot(pergunta01_12) +
   ) +
   labs(x = "Sexo", y = "Frequência") +
   theme_estat()+
-  scale_y_continuous(breaks = seq(from = 0, to = 40, by = 10), limits=c(0, 40))
+  scale_y_continuous(breaks = seq(from = 0, to = 50, by = 10), limits=c(0, 50))
 ggsave(filename = file.path(caminho_lucas, "colunas-uni-freq-01-12.pdf"), width = 158, height = 93, units = "mm")
 
 
@@ -229,19 +237,21 @@ unique(vetor_pergunta_14)
 #Vetor com todos os valores unicos que aparecem no vetor_pergunta_14
 
 # Definir o vetor de lesao
-lesao <- c(
-  "Lesão ligamentar",                                                     
-  "Estiramento muscular",                                                 
-  "Tendinite",                                                            
-  "Degenerações do disco intervertebral",                                 
-  "Dor crônica na coluna",                                                
-  "Desgaste/dor articular",                                               
-  "Lesão completa do ligamento do Escafóide-Semilunar no punho esquerdo", 
-  "Hérnia inguinal (apesar de não estar incluído em musculoesqueléticas)",
-  "Protrusão discal",                                                     
-  "Lombalgia",                                                            
-  "Condromalácia patelar" 
-)
+lesao <- c("Lesão ligamentar",
+           "Dor crônica na coluna",
+           "Estiramento muscular",
+           "Tendinite",
+           "Condromalácia patelar",
+           "Degenerações do disco intervertebral",
+           "Desgaste/dor articular",
+           "Hérnia inguinal (apesar de não estar incluído em musculoesqueléticas)",
+           "Protrusão discal",
+           "Lombalgia",
+           "Fascite plantar",
+           "Síndrome do túnel do carpo",
+           "Desconforto na lombar",
+           "hérnia de disco")
+
 
 # Inicializar um vetor para armazenar as contagens
 contagens_lesao <- integer(length(lesao))
@@ -264,7 +274,7 @@ contagens_lesao_df <- data.frame(lesao = lesao, quantidade = contagens_lesao)
 
 
 contagens_lesao_df %>%
-  # filter(quantidade > 2) %>%
+   # filter(quantidade > 2) %>%
   mutate(
     freq = quantidade/sum(quantidade)
   ) %>%
@@ -286,10 +296,13 @@ ggplot() +
   ) +
   labs(x = "Lesão", y = "Frequência") +
   theme_estat()+
-  scale_y_continuous(breaks = seq(from = 0, to = 20, by = 5), limits=c(0, 20))+
+  scale_y_continuous(breaks = seq(from = 0, to = 30, by = 5), limits=c(0, 30))+
   scale_x_discrete(guide = guide_axis(n.dodge = 2))+
   theme(axis.text.x = element_text(size = 8))
 ggsave(filename = file.path(caminho_lucas, "colunas-uni-freq-14.pdf"), width = 158, height = 93, units = "mm")
+
+
+
 
 
 
@@ -335,7 +348,7 @@ ggplot(pergunta7_12) +
   ) +
   labs(x = "Pratica de Atividade Física", y = "Frequência") +
   theme_estat()+
-  scale_y_continuous(breaks = seq(from = 0, to = 50, by = 10), limits=c(0, 50))
+  scale_y_continuous(breaks = seq(from = 0, to = 70, by = 10), limits=c(0, 70))
 ggsave(filename = file.path(caminho_lucas, "colunas-uni-freq-07-12.pdf"), width = 158, height = 93, units = "mm")
 
 
@@ -377,7 +390,7 @@ ggplot(pergunta_08_12) +
   ) +
   labs(x = "Treinamento para minimizar os riscos de lesões", y = "Frequência") +
   theme_estat()+
-  scale_y_continuous(breaks = seq(from = 0, to = 50, by = 10), limits=c(0, 50))
+  scale_y_continuous(breaks = seq(from = 0, to = 70, by = 10), limits=c(0, 70))
 ggsave(filename = file.path(caminho_lucas, "colunas-uni-freq-08-12.pdf"), width = 158, height = 93, units = "mm")
 
 
@@ -441,4 +454,221 @@ ggplot(pergunta05_12) +
   scale_x_discrete(guide = guide_axis(n.dodge = 2))+
   theme(axis.text.x = element_text(size = 8))
 ggsave(filename = file.path(caminho_lucas, "colunas-uni-freq-05-12.pdf"), width = 158, height = 93, units = "mm")
+
+
+
+#Ela quer saber também porcentagem dos que precisaram ser afastados(Pergunta 17)
+#Então dos 70 que responderam a pergunta 17 54 (77,14%) respondeu que precisou ser afastado 
+pergunta17 <- cad_andressa %>% 
+  select(Pergunta_17) %>% 
+  filter(!is.na(Pergunta_17)) %>% 
+  group_by(Pergunta_17) %>% 
+  summarise(freq = n()) %>%
+  mutate(
+    freq_relativa = freq %>% percent()
+  )
+
+
+
+#-------Relação entre a prática de atividade física com o fato sentir dor/desconforto-------
+#gráfico "extra"
+
+#Pergunta07: Pratica alguma atividade física
+#Pergunta09: Sente dor ou desconforto 
+
+
+pergunta_07_09 <- cad_andressa %>% 
+  select(Pergunta_07,Pergunta_09) %>%
+  group_by(Pergunta_07, Pergunta_09) %>%
+  summarise(freq = n()) %>%
+  mutate(
+    freq_relativa = freq %>% percent()
+  )
+
+colnames(pergunta_07_09) <- c("Pergunta_07",
+                              "Sente Dor",
+                              "freq",
+                              "freq_relativa"
+                              
+  
+)
+
+
+porcentagens <- str_c(pergunta_07_09$freq_relativa, "%") %>% str_replace("\\.", ",")
+
+legendas <- str_squish(str_c(pergunta_07_09$freq, " (", porcentagens, ")"))
+
+ggplot(pergunta_07_09) +
+  aes(
+    x = fct_reorder(Pergunta_07, freq, .desc = T),
+    y = freq,
+    fill = `Sente Dor`,
+    label = legendas
+  ) +
+  geom_col(position = position_dodge2(preserve = "single", padding = 0)) +
+  geom_text(
+    position = position_dodge(width = .9),
+    vjust = -0.5, hjust = 0.5,
+    size = 3
+  ) +
+  labs(x = "Pratica de Atividade Física", y = "Frequência") +
+  theme_estat()+
+  scale_y_continuous(breaks = seq(from = 0, to = 70, by = 10), limits=c(0, 70))
+ggsave(filename = file.path(caminho_lucas, "colunas-uni-freq-07-09.pdf"), width = 158, height = 93, units = "mm")
+
+
+
+
+
+
+#---------------------Movimentos que favorecem dor/desconforto------------------
+#Pergunta 11: Quais movimentos favorecem a dor/desconforto?
+
+# Filtrar a pergunta 11
+pergunta11 <- cad_andressa %>% 
+  select(Pergunta_11) %>% 
+  filter(!is.na(Pergunta_11))
+
+# Extraindo todas as informações das linhas e colocando em um vetor para identificar todos os valores diferentes
+vetor_pergunta_11 <- unlist(strsplit(pergunta11$Pergunta_11, ", "))
+print(vetor_pergunta_11)
+unique(vetor_pergunta_11)
+
+# Vetor com todos os valores únicos que aparecem no vetor_pergunta_11
+
+# Definir o vetor de movimentos de desconforto
+mov_desconf <- c("Ao elevar a maca/prancha com a vítima",
+                 "Ao elevar/transportar a vítima em seus braços",
+                 "Na posição de imobilização da coluna cervical da vítima",
+                 "Na imobilização de membros",
+                 "Ao realizar RCP",
+                 "Ao realizar movimentações de rolamento e elevação da vítima (à cavalero)",
+                 "Ao realizar algum movimento de apoio do corpo da vitima dentro da UR.",
+                 "Durante o transporte de vítimas em prédios sem elevador.",
+                 "Muito tempo abaixado ou de joelhos",
+                 "Mudar paciente de maca.",
+                 "Ao final do dia após ter passado muito tempo sentada (devido ao banco dianteiro da UR)",
+                 "Ao descer e subir na viatura",
+                 "Subindo e descendo da viatura")
+
+# Inicializar um vetor para armazenar as contagens
+contagens_mov_desconf <- integer(length(mov_desconf))
+
+# Loop for para contar as correspondências usando expressões regulares
+for (i in 1:length(mov_desconf)) {
+  contagens_mov_desconf[i] <- sum(sapply(pergunta11, function(x) str_detect(x, fixed(mov_desconf[i]))))
+}
+
+# Criar um novo dataframe para exibir as contagens
+contagens_mov_desconf_df <- data.frame(mov_desconf = mov_desconf, quantidade = contagens_mov_desconf)
+
+
+# Vetor com as abreviações correspondentes
+abreviacoes <- c(
+  "Maca/Prancha",
+  "Elevar/Transportar",
+  "Imobilização Cervical",
+  "Imobilização de Membros",
+  "RCP",
+  "Movimentações à Cavalero",
+  "Ao realizar algum movimento de apoio do corpo da vitima dentro da UR.",
+  "Durante o transporte de vítimas em prédios sem elevador.",
+  "Muito tempo abaixado ou de joelhos",
+  "Mudar paciente de maca.",
+  "Ao final do dia após ter passado muito tempo sentada (devido ao banco dianteiro da UR)",
+  "Ao descer e subir na viatura",
+  "Subindo e descendo da viatura"
+)
+
+contagens_mov_desconf_df$mov_desconf <- abreviacoes
+
+
+#plotando o gráfico
+contagens_mov_desconf_df <- contagens_mov_desconf_df %>%
+  mutate(
+    freq = quantidade/sum(quantidade)
+  ) %>%
+  mutate(
+    freq = gsub("\\.", ",", scales::percent(freq)),
+    label = paste(quantidade, " (", freq, ")", sep = "")
+  )
+
+
+#Foram retirados os movimentos de dor e desconforto que foram listados apenas 1 vez pois representavam menos de 1% 
+contagens_mov_desconf_df %>% 
+  filter(quantidade > 1) %>% 
+  ggplot() +
+  aes(
+    x = fct_reorder(mov_desconf, quantidade, .desc = T),
+    y = quantidade,
+    label = label
+  ) +
+  geom_bar(stat = "identity", fill = "#A11D21", width = 0.7) +
+  geom_text(
+    position = position_dodge(width = .9),
+    vjust = -0.5, # hjust = .5,
+    size = 3
+  ) +
+  labs(x = "Movimentos que Favorecem Dor/Desconforto", y = "Frequência") +
+  theme_estat()+
+  scale_x_discrete(guide = guide_axis(n.dodge = 2))+
+  theme(axis.text.x = element_text(size = 7))
+ggsave(filename = file.path(caminho_lucas, "colunas-uni-freq-11.pdf"), width = 158, height = 93, units = "mm")
+
+  
+
+
+
+
+#----------Relação de quem respondeu “SIM” para questão 9 com a questão 15, além de correlacionar com a prática de atividade física.-------
+#filtrar pergunta 9 e relacionar pergunta 15 com pergunta 7
+
+
+pergunta_07_15 <- cad_andressa %>% 
+  select(Pergunta_09, Pergunta_07, Pergunta_15) %>% 
+  filter(Pergunta_09 == "Sim", !is.na(Pergunta_15))
+
+#125 pessoas responderam Sim para a pergunta 09, entretanto 63 pessoas deixaram em branco a pergunta 15, restando então 62 observações para relacionar a pergunta 07 com a pergunta 15
+
+
+pergunta_07_15 <- pergunta_07_15 %>%
+    mutate(Pergunta_15 = case_when(
+      Pergunta_15 %>% str_detect("Sim. E piorou após atuar na UR.") ~ "Sim. Piorou após UR",
+      Pergunta_15 %>% str_detect("Sim. E manteve após atuar na UR.") ~ "Sim. Manteve após UR.",
+      Pergunta_15 %>% str_detect("Não possuía.") ~ "Não possuía.",
+      Pergunta_15 %>% str_detect("Não sei.") ~ "Não sei."
+  )) %>%
+  group_by(Pergunta_15, Pergunta_07) %>%
+  summarise(freq = n()) %>%
+  mutate(
+    freq_relativa = freq %>% percent()
+  )
+
+colnames(pergunta_07_15)[2] <- "Pratica de Atividade Física"
+
+porcentagens <- str_c(pergunta_07_15$freq_relativa, "%") %>% str_replace("\\.", ",")
+
+legendas <- str_squish(str_c(pergunta_07_15$freq, " (", porcentagens, ")"))
+
+ggplot(pergunta_07_15) +
+  aes(
+    x = fct_reorder(Pergunta_15, freq, .desc = T),
+    y = freq,
+    fill = `Pratica de Atividade Física`,
+    label = legendas
+  ) +
+  geom_col(position = position_dodge2(preserve = "single", padding = 0)) +
+  geom_text(
+    position = position_dodge(width = .9),
+    vjust = -0.5, hjust = 0.45,
+    size = 2
+  ) +
+  labs(x = "Histórico de Dores Antes de Trabalhar na UR", y = "Frequência") +
+  theme_estat()+
+  theme(axis.text.x = element_text(size = 8),
+        legend.text = element_text(size = 8))
+
+ggsave(filename = file.path(caminho_lucas, "colunas-uni-freq-15-07.pdf"), width = 158, height = 93, units = "mm")
+
+
 
